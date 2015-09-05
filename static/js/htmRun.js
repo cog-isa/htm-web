884f8e7a8126f0ac5a292db438124db47defb5d5
tviskaron <tviskaron@gmail.com>
2015-09-05 17:37:02 +0300

    переделка gui - отображения работы алгоритма

--- static/js/htmRun.js
+++ static/js/htmRun.js
@@ -13,103 +13,107 @@ function clear_element(element_id) {
 var json_store;
 
 
+function draw_input_data() {
+    clear_element("input_data");
+
+    for (var i = 0; i < json_store["input"].length; i++) {
+        for (var j = 0; j < json_store["input"][i].length; j++) {
+
+            var cell_str1 = '<a class="thumbnail htm_ceil"></a>';
+            if (json_store["input"][i][j] == 1)
+                cell_str1 = '<a class="thumbnail btn-success htm_ceil"></a>';
+            var cell_elem2 = create_element("cell_", cell_str1);
+
+            document.getElementById("input_data").appendChild(cell_elem2);
+        }
+        var tr1 = '<br>';
+        var tr_element1 = create_element("column_", tr1);
+        document.getElementById("input_data").appendChild(tr_element1);
+    }
+}
+
+function draw_compress_data() {
+    clear_element("compress_input");
+
+    console.log(json_store["compress_input"]);
+    for (var i = 0; i < json_store["compress_input"].length; i++) {
+        for (var j = 0; j < json_store["compress_input"][i].length; j++) {
+
+            var cell_str11 = '<a class="thumbnail htm_ceil"></a>';
+            if (json_store["compress_input"][i][j] == 1)
+                cell_str11 = '<a class="thumbnail btn-success htm_ceil"></a>';
+            var cell_elem22 = create_element("cell_", cell_str11);
+
+            document.getElementById("compress_input").appendChild(cell_elem22);
+        }
+        var tr12 = '<br>';
+        var tr_element12 = create_element("column_", tr12);
+        document.getElementById("compress_input").appendChild(tr_element12);
+    }
+}
+
+function draw_temporal_pool() {
+    clear_element("viz");
+    var region_size = json_store["temporal_pooler"]["region_size"];
+    var cells_size = json_store["temporal_pooler"]["columns"][0][0]["cells"].length;
+    for (var i = 0; i < region_size; i++) {
+
+        for (var j = 0; j < region_size; j++) {
+            var column_str = '<div id="region_" class="thumbnail htm_column" ></div>';
+            var column = create_element("column_", column_str);
+            document.getElementById("viz").appendChild(column);
+            for (var k = 0; k < cells_size; k++) {
+
+                var cell_str = '';
+                var id = json_store["temporal_pooler"]["columns"][i][j]["cells"][k]["id"];
+                if (json_store["temporal_pooler"]["columns"][i][j]["cells"][k]["state"] == 1)
+                    cell_str = '<a onclick="show_dendrites(' + id + ')" id = "' + id + '"class="thumbnail htm_cell"></a>';
+                if (json_store["temporal_pooler"]["columns"][i][j]["cells"][k]["state"] == 2)
+                    cell_str = '<a onclick="show_dendrites(' + id + ')" id = "' + id + '"class="thumbnail htm_cell btn-success"></a>';
+                if (json_store["temporal_pooler"]["columns"][i][j]["cells"][k]["state"] == 3)
+                    cell_str = '<a onclick="show_dendrites(' + id + ')" id = "' + id + '"class="thumbnail htm_cell btn-warning"></a>';
+
+                var cell_elem = create_element("cell_", cell_str);
+                column.appendChild(cell_elem);
+            }
+        }
+        var tr = '<br>';
+        var tr_element = create_element("column_", tr);
+        document.getElementById("viz").appendChild(tr_element);
+    }
+}
+
 function do_go() {
     $.post('/turn_on_java_server/', {
         text: "hello"
     }).done(function (json) {
             json_store = json;
-            clear_element("viz");
-            //console.log(json["temporal_pooler"]);
-            var region_size = json["temporal_pooler"]["region_size"];
-            var cells_size = json["temporal_pooler"]["columns"][0][0]["cells"].length;
-            for (var i = 0; i < region_size; i++) {
-
-                for (var j = 0; j < region_size; j++) {
-                    //debug_add_line(json["temporal_pooler"]["columns"][i][j]);
-                    var column_str = '<div id="region_" class="thumbnail" style="display: inline-block; overflow-x: auto; overflow-y: hidden; margin: 0; height:50px; width:50px;"></div>';
-                    var column = create_element("column_", column_str);
-                    document.getElementById("viz").appendChild(column);
-                    for (var k = 0; k < cells_size; k++) {
-
-                        var cell_str = '';
-                        var id = json["temporal_pooler"]["columns"][i][j]["cells"][k]["id"];
-                        if (json["temporal_pooler"]["columns"][i][j]["cells"][k]["state"] == 1)
-                            cell_str = '<a onclick="show_dendrites(' + id + ')" id = "' + id + '"class="thumbnail"style="display: inline-flex; overflow-x: auto;  margin: 0"></a>';
-                        if (json["temporal_pooler"]["columns"][i][j]["cells"][k]["state"] == 2)
-                            cell_str = '<a onclick="show_dendrites(' + id + ')" id = "' + id + '"class="thumbnail btn-success"style="display: inline-flex; overflow-x: auto;  margin: 0"></a>';
-                        if (json["temporal_pooler"]["columns"][i][j]["cells"][k]["state"] == 3)
-                            cell_str = '<a onclick="show_dendrites(' + id + ')" id = "' + id + '"class="thumbnail btn-warning"style="display: inline-flex; overflow-x: auto;  margin: 0"></a>';
-
-                        var cell_elem = create_element("cell_", cell_str);
-                        column.appendChild(cell_elem);
-                    }
-                }
-                var tr = '<br>';
-                var tr_element = create_element("column_", tr);
-                document.getElementById("viz").appendChild(tr_element);
-            }
-
-            clear_element("input_data");
-
-            console.log(json["input"]);
-            for (var i = 0; i < json["input"].length; i++) {
-                for (var j = 0; j < json["input"][i].length; j++) {
-
-                    var cell_str1 = '<a href="PASSIVE" class="thumbnail"style="display: inline-flex; overflow-x: auto;  margin: 2px"></a>';
-                    if (json["input"][i][j] == 1)
-                        cell_str1 = '<a href="ACTIVE" class="thumbnail btn-success"style="display: inline-flex; overflow-x: auto;  margin: 2px"></a>';
-                    var cell_elem2 = create_element("cell_", cell_str1);
-
-                    document.getElementById("input_data").appendChild(cell_elem2);
-                }
-                var tr1 = '<br>';
-                var tr_element1 = create_element("column_", tr1);
-                document.getElementById("input_data").appendChild(tr_element1);
-            }
-
-
-            clear_element("compress_input");
-
-            console.log(json["compress_input"]);
-            for (var i = 0; i < json["compress_input"].length; i++) {
-                for (var j = 0; j < json["compress_input"][i].length; j++) {
-
-                    var cell_str11 = '<a href="PASSIVE" class="thumbnail"style="display: inline-flex; overflow-x: auto;  margin: 2px"></a>';
-                    if (json["compress_input"][i][j] == 1)
-                        cell_str11 = '<a href="ACTIVE" class="thumbnail btn-success"style="display: inline-flex; overflow-x: auto;  margin: 2px"></a>';
-                    var cell_elem22 = create_element("cell_", cell_str11);
-
-                    document.getElementById("compress_input").appendChild(cell_elem22);
-                }
-                var tr12 = '<br>';
-                var tr_element12 = create_element("column_", tr12);
-                document.getElementById("compress_input").appendChild(tr_element12);
-            }
-
+            draw_input_data();
+            draw_compress_data();
+            draw_temporal_pool();
         }
     )
     ;
 }
 
 function get_cell_by_id(id) {
-    var json = json_store;
-    var region_size = json["temporal_pooler"]["region_size"];
-    var cells_size = json["temporal_pooler"]["columns"][0][0]["cells"].length;
+    var region_size = json_store["temporal_pooler"]["region_size"];
+    var cells_size = json_store["temporal_pooler"]["columns"][0][0]["cells"].length;
     for (var i = 0; i < region_size; i++)
         for (var j = 0; j < region_size; j++)
             for (var k = 0; k < cells_size; k++)
-                if (id == json["temporal_pooler"]["columns"][i][j]["cells"][k]["id"])
-                    return json["temporal_pooler"]["columns"][i][j]["cells"][k];
+                if (id == json_store["temporal_pooler"]["columns"][i][j]["cells"][k]["id"])
+                    return json_store["temporal_pooler"]["columns"][i][j]["cells"][k];
 }
 
 function clear_all_dendrites() {
     var json = json_store;
-    var region_size = json["temporal_pooler"]["region_size"];
-    var cells_size = json["temporal_pooler"]["columns"][0][0]["cells"].length;
+    var region_size = json_store["temporal_pooler"]["region_size"];
+    var cells_size = json_store["temporal_pooler"]["columns"][0][0]["cells"].length;
     for (var i = 0; i < region_size; i++)
         for (var j = 0; j < region_size; j++)
             for (var k = 0; k < cells_size; k++) {
-                var id_to = json["temporal_pooler"]["columns"][i][j]["cells"][k]["id"];
+                var id_to = json_store["temporal_pooler"]["columns"][i][j]["cells"][k]["id"];
                 document.getElementById(id_to).style.border = "1px solid #ddd";
             }
 }

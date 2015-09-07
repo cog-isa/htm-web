6d4a4e5554cf3ea85e0a1b417178ca247fc6b8fc
DIdro <gmdidro@gmail.com>
2015-09-07 11:38:48 +0300

    Реализовано добавление\удаление настроек в htmSettings

--- static/js/htmSettings.js
+++ static/js/htmSettings.js
@@ -29,14 +29,3 @@ $( document ).ready(function() {
 });
 */
 
-
-$('#myButton').on('click', function () {
- $.post('/add_new_conf/', {
-        text: "hello"
-    }).done(function (json) {
-
-    }).fail(function() {
-        alert( "error" );
-    })
-},
-dataType="json")
